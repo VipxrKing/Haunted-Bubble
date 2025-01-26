dff873b65f199d3588d45881078a02993163295b
Tamaldar Komaleuz 3er Cristo Desredentor Plus <70250872+Rikisimo69@users.noreply.github.com>
2025-01-25 19:57:04 -0600

    sis

--- Scenes/note.gd
+++ Scenes/note.gd
@@ -3,4 +3,7 @@ extends Node3D
 @export_multiline var note_text:String
 
 func _on_interactable_interacted() -> void:
-	pass # Replace with function body.
+	Utils.get_player().INPUT = false
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	Utils.get_ui().find_child("HUD").find_child("Note").text = note_text
+	Utils.get_ui().find_child("HUD").find_child("Note").visible = true
