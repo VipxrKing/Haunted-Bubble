37b18c90cf7d68382e763461ebfa8f2de32b9bf0
Bryan Dardon <brdardon@gmail.com>
2025-01-26 16:16:55 -0600

    FinalDoor
    Puerta final

--- Scenes/player_detector.gd
+++ Scenes/player_detector.gd
@@ -3,10 +3,10 @@ extends Area3D
 signal player_entered
 signal player_exited
 
-func _on_body_entered(body):
+func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		emit_signal("player_entered")
 
-func _on_body_exited(body):
+func _on_body_exited(body: Node3D) -> void:
 	if body is Player:
 		emit_signal("player_exited")
